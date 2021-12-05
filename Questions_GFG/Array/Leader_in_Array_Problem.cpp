a7c488424a1a4b591fc37598db4f5702e0e99632
Ashish <ashjha2002@gmail.com>
2021-12-05 17:05:16 +0530

    Leader in Array Problem Added

---
+++ Questions_GFG/Array/Leader_in_Array_Problem.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void leaders(int arr[], int n){ // Naive Method
+	for(int i = 0; i < n; i++){
+		bool flag = false;
+		for(int j = i + 1; j < n; j++)
+			if(arr[i] <= arr[j]){
+				flag = true;
+				break;
+			}
+
+		if(flag == false)   cout<<arr[i]<<" ";
+	}
+}
+
+void leaders(int arr[], int n){ // Efficient Method
+	int curr_ldr = arr[n - 1];
+	cout<<curr_ldr<<" ";
+	for(int i = n - 2; i >= 0; i--)
+		if(curr_ldr < arr[i]){
+			curr_ldr = arr[i];
+			cout<<curr_ldr<<" ";
+		}
+}
+
+int main() {
+    int arr[] = {7, 10, 4, 10, 6, 5, 2}, n = 7;
+    leaders(arr, n);
+    return 0;
+}
